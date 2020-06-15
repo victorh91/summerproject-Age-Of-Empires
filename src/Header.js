90f10fcc526cf5cabb9cae8fabba7d3ae26fc618
victorh91 <vrha0001@stud.slu.se>
2020-06-15 19:52:08 +0200

    First commit

---
+++ src/Header.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import logo from './images.png';
+
+const Header = () => (
+    <header>
+    <Navbar className ="color-nav" collapseOnSelect expand="lg" >
+      <Navbar.Brand href="aoe.jpg">
+      <img
+        img src={logo} 
+        width="80"
+        height="60"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Nav>
+          <Nav.Link href="#deets">More deets</Nav.Link>
+          <Nav.Link eventKey={2} href="#memes">
+            Dank memes
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    </header>
+);
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,83 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Header from './Header.js';
+import $ from "jquery"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      civil: []
+    };
+  }
+  componentDidMount() {
+    fetch("https://cors-anywhere.herokuapp.com/https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            civil: result.civilizations           
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+  displayOptions = () => {
+    let empires = []
+    if (this.state.civil != null) {
+      for (let i = 0; i < this.state.civil.length; i++) {
+        let names = []
+        let army = []
+        let teamBonus = []
+        names.push(`${this.state.civil[i].name}`)
+        army.push(`${this.state.civil[i].army_type}`)
+        teamBonus.push(`${this.state.civil[i].team_bonus}`)
+        empires.push(<div className="civil" id = {i} key={i}><h2>{names}</h2><h5>Specialty: {army}</h5>
+        <h5>Team bonus: {teamBonus}</h5> <Button  onClick={() => this.handleClick(i) } variant="success">Add</Button>{' '}
+        </div>)
+      }
+      return empires
+    }
+  }
+  handleClick = (id) => {
+    let list = JSON.parse(localStorage.getItem("civil") || "[]");
+    list.push(id)
+    console.log(list)
+    localStorage.setItem("civil", JSON.stringify(list));
+    list = []
+    console.log(list)
+  }
+  displayIntro = () => {
+    let intro = []
+    intro.push(
+    <div className="intro" key="intro">
+      <h1 id="welcome">Welcome to Age Of Empires 2</h1> <br></br>
+      <h5>Add civilizations to your library</h5>
+    </div>)
+    return intro
+  }
+  render() {
+    return (<div className="title"> 
+    <Header />
+    {this.displayIntro()}
+    {this.displayOptions()}
+    </div>  
+    )
+    
+  }
+  
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDOM.render(<Main />, document.getElementById('root'));
